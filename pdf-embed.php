23a407ecbcbf4ce0965cadd82bc8a5d22fac4254
Tropicalista <francescopepe@email.it>
2023-05-18 06:26:40 +0200

    fix version number

--- pdf-embed.php
+++ pdf-embed.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pdf Embed
  * Plugin URI:  https://formello.net/
  * Description: PDF embedded with official Adobe API.
- * Version:     0.3.3
+ * Version:     0.3.4
  * Author:      Formello
  * Author URI:  https://formello.net
  * License:     GPL2
